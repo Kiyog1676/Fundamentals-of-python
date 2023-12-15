ea3cb9896a6b783e4eb10473d7e3f59a68e8cc1e
Kiyog1676 <139625108+Kiyog1676@users.noreply.github.com>
2023-12-15 06:58:54 -0500

    Delete 6_sum_of_positive_integers.py

--- 6_sum_of_positive_integers.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter a number to calculate the sum of positive integers: "))
-sum_positive = 0
-
-for i in range(1, n + 1):
-    print(i)
-    if i > 0:
-        sum_positive += i
-
-print("Sum of positive integers =", sum_positive)
